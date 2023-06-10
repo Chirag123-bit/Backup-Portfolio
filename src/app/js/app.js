9ffa6cbfcc468599ad30fbd505d5f9ccd2976017
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-06-10 19:58:23 +0545

    Removed Unused Code

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -153,52 +153,7 @@
     }
   }
 
-  // function init_contact_form() {
-  //   var $el = $("#contact-form");
-  //   var $alert_wrap = $("#contact-form-alert");
 
-  //   if ($el.length && $alert_wrap.length) {
-  //     $el.on("submit", function() {
-  //       var $btn = $("#btn-contact-form");
-  //       var params = $el.serialize();
-
-  //       init_btn_loading($btn, true);
-
-
-  //       $.post("src/php/sendmail.php", params, function(data) {
-  //         var dt = JSON.parse(data);
-  //         if (dt.status == "error") {
-  //           var alert = init_alert(
-  //             "contact-alert-err",
-  //             dt.status_desc,
-  //             "uk-alert-danger",
-  //             "warning"
-  //           );
-  //         } else {
-  //           var alert = init_alert(
-  //             "contact-alert-success",
-  //             dt.status_desc,
-  //             "uk-alert-primary",
-  //             "info"
-  //           );
-  //           $el.trigger("reset");
-  //         }
-  //         $.each(dt.error_msg, function(key, value) {
-  //           if (value == "") {
-  //             $("#" + key).removeClass("uk-form-danger");
-  //           } else {
-  //             $("#" + key).addClass("uk-form-danger");
-  //           }
-  //           $("#" + key + "_error").html(value);
-  //         });
-  //         $alert_wrap.html(alert);
-  //         init_btn_loading($btn, false);
-  //       });
-
-  //       return false;
-  //     });
-  //   }
-  // }
 
   function init_btn_loading($btn, is_loading) {
     if (is_loading) {
